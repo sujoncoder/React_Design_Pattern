9d4aef2290e9a22cf5d39ad87d424a9e23229879
sujoncoder <sujonsheikh.in@gmail.com>
2025-02-27 23:33:51 -0800

    start react design pattern

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-7xl font-bold text-center my-10'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
